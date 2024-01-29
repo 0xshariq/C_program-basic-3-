02d7ff24aa08ede9d736d70bcb0e82edd9dc86cc
0xshariq <145042629+0xshariq@users.noreply.github.com>
2024-01-29 15:51:44 +0000

    New

--- simple_interest.c
+++ simple_interest.c
@@ -1,20 +1,12 @@
-// C program to implement 
-// the above approach 
-#include <stdio.h> 
-  
-// Driver code 
+#include <stdio.h>   
 int main() 
 { 
-    // We can change values here for 
-    // different inputs 
-    float P, R, T; 
-    // Assigning the values at run time
+    float P, R, T;
     printf("Enter the Principal Amount, Rate, Time :");
   scanf("%f %f %f",&P,&R,&T);
-    // Calculate simple interest 
+
     float SI = (P * T * R) / 100; 
-  
-    // Print Simple Interest 
+
     printf("Simple Interest = %f\n", SI); 
   
     return 0; 
