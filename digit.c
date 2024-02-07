90678c3b86a2281cb629d634eda1545ff1c8d0d7
0xshariq <145042629+0xshariq@users.noreply.github.com>
2024-02-07 16:21:32 +0000

    N

---
+++ digit.c
@@ -0,0 +1,18 @@
+// C program to check given character is a digit or not
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char character;
+
+    printf("Enter a character: ");
+    scanf("%c", &character);
+
+    if (isdigit(character)) {
+        printf("%c is a digit.\n", character);
+    } else {
+        printf("%c is not a digit.\n", character);
+    }
+
+    return 0;
+}
